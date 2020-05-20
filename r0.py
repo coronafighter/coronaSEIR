fix calculation of r0 was completely wrong before, hopefully OK now

r0.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import world_data
 import shared
 
+INTERVAL = 4  # days
 
 COUNTRY = 'all'
 PROVINCE = 'all'
@@ -15,13 +16,11 @@ EXCLUDECOUNTRIES = ['China'] if COUNTRY == 'all' else []  # massive measures ear
 XCDR_data = np.array(world_data.get_country_xcdr(COUNTRY, PROVINCE,
                                                  excludeCountries=EXCLUDECOUNTRIES, returnDates=True))
 
-
 C = XCDR_data[:,1]  # cases
 D = XCDR_data[:,2]  # deaths
 
-
-INTERVAL = 4  # days
-
+deltaC = np.diff(C)  # new per day
+deltaD = np.diff(D)
 
 def calc_R(Y):
     R = []
@@ -36,17 +35,24 @@ def calc_R(Y):
         R.append(r)
     return R
 
-cR = calc_R(C)
-dR = calc_R(D)
+smoothDeltaC = shared.moving_average(deltaC, n=7)  # smooth out weekly swing
+smoothDeltaD = shared.moving_average(deltaD, n=7)
 
-cR7 = shared.moving_average(cR, n=7, noRightZero=True)
-dR7 = shared.moving_average(dR, n=7, noRightZero=True)
+cR = calc_R(smoothDeltaC)
+dR = calc_R(smoothDeltaD)
 
-print("current 7 day moving averages R: cases %.2f   deaths %.2f" % (cR7[-5], dR7[-5]))
+smoothCR = shared.moving_average(cR, n=1)
+smoothDR = shared.moving_average(dR, n=1)
+
+print("experimental!")
+print("assumed interval:", INTERVAL)
+print("filtered moving averages R: cases %.2f   deaths %.2f" % (smoothCR[-1], smoothDR[-1]))
 
 fig = plt.figure(dpi=75, figsize=(20,16))
 ax = fig.add_subplot(111)
+ax.set_ylim(top=7.0)
 ax2 = ax.twinx()
+ax2.set_ylabel('NEW PER DAY')
 ax2.set_yscale("log", nonposy='clip')
 
 ax.grid(linestyle=':')
@@ -54,10 +60,13 @@ ax.grid(linestyle=':')
 ax.plot(cR, color='orange', alpha=0.2, lw=1)
 ax.plot(dR, color='black', alpha=0.2, lw=1)
 
-ax.plot(cR7, color='orange', alpha=0.5, lw=2, marker=None)
-ax.plot(dR7, color='black', alpha=0.5, lw=2, marker=None)
+ax.plot(smoothCR, color='orange', alpha=0.5, lw=2, marker=None)
+ax.plot(smoothDR, color='black', alpha=0.5, lw=2, marker=None)
 
+ax2.plot(deltaC, marker='o', color='orange', alpha=0.5, lw=0)
+ax2.plot(deltaD, marker='x', color='black', alpha=0.5, lw=0)
 
-ax2.plot(C, marker='o', color='orange', alpha=0.5, lw=1)
-ax2.plot(D, marker='x', color='black', alpha=0.5, lw=1)
+ax2.plot(smoothDeltaC, marker='', color='orange', alpha=0.5, lw=1)
+ax2.plot(smoothDeltaD, marker='', color='black', alpha=0.5, lw=1)
+
 plt.show()